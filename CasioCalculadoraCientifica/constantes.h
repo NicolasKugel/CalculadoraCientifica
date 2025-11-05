24bbcc59114f91df0f327e8d3b07c0fe236b36f9
juaniortiz96 <juaniortiz96@gmail.com>
2025-11-05 02:50:14 -0300

    Adaptacion de nuevas validaciones y edicion de la ecuacion

--- CasioCalculadoraCientifica/constantes.h
+++ CasioCalculadoraCientifica/constantes.h
@@ -7,6 +7,13 @@
 #define LONG_DE_SESIONES 51
 #define LONG_NOMBRE_DE_ARCHIVO 51
 #define RUTA_COMPLETA_ARCHIVO 66
-
+/* =====================================================================
+   CONFIGURACION / CONSTANTES
+   ---------------------------------------------------------------------
+   - MAXIMO_LARGO_ECUACION: tope logico para leer una ecuacion desde stdin
+   - RUTA_ARCHIVO_TEMPORAL: ruta por defecto para guardar las ecuaciones
+   ===================================================================== */
+#define MAXIMO_LARGO_ECUACION 256
+#define RUTA_ARCHIVO_TEMPORAL "ecuaciones/ecuaciones-sesion-actual.tmp"
 
 #endif // CONSTANTES_INCLUDED
