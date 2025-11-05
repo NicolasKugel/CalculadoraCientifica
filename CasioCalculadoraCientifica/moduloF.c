eea4ed842bc45dd71c372ead48604e5e1e9a3885
NicoSL96 <nicolas.sebastianledezma@gmail.com>
2025-11-05 19:23:02 -0300

    actualizacion e integracion del modulo F al programa

--- CasioCalculadoraCientifica/moduloF.c
+++ CasioCalculadoraCientifica/moduloF.c
@@ -1,11 +1,65 @@
 #include "moduloF.h"
 
+typedef struct {
+    char fn[LONG_DE_ECUACIONES];
+    int pos;
+} tbl_fn;
+
+typedef struct {
+    double x;
+    double y;
+    char* result;
+} tbl_val;
+
+
+char* __seleccionarFuncion()
+{
+    tbl_fn funciones[CANTIDAD_DE_ECUACIONES] = {{"\0", 1},{"\0", 2},{"\0", 3},{"\0", 4},{"\0", 5},{"\0", 6},{"\0", 7},{"\0", 8},{"\0", 9},{"\0", 10}};
+    FILE *sesion_actual = fopen("ecuaciones/ecuaciones-sesion-actual.tmp", "r");
+    tbl_fn *fn_aux = funciones;
+    char *output = (char*) malloc(LONG_DE_ECUACIONES);
+    int j = 1, opcionValida = 1;
+    while(fgets(fn_aux->fn, LONG_DE_ECUACIONES, sesion_actual))
+    {
+        fn_aux++;
+    }
+    rewind(sesion_actual);
+    fn_aux = funciones;
+    do
+    {
+        clearScreen();
+        if(!opcionValida)
+            printf("opcion invalida, por favor seleccione una correcta\n\n");
+        fn_aux = funciones;
+        printf("Lista de ecuaciones: \n");
+        while(strcmp(fn_aux->fn,"\0"))
+        {
+            printf("%d) %s\n", fn_aux->pos, fn_aux->fn);
+            fn_aux++;
+        }
+        printf("\nSeleccione una opcion: ");
+        scanf("%d", &j);
+        while (getchar() != '\n');
+        fn_aux = funciones;
+        if(j>=1 && j<=10)
+        {
+            while(fn_aux->pos != j)
+            fn_aux++;
+        }
+        opcionValida = 0;
+    } while(!strcmp(fn_aux->fn,"\0") || (j<1 || j>10));
+    strcpy(output,fn_aux->fn);
+    fclose(sesion_actual);
+    return output;
+}
+
 int __cargarTablaManual(tbl_val **tbl)
 {
     int i, fls = 0;
     clearScreen();
     printf("Ingrese cuantos casos desea evaluar: ");
     scanf("%d", &fls);
+    while (getchar() != '\n');
     *tbl = (tbl_val*) malloc(sizeof(tbl_val) * fls);
     tbl_val *aux = *tbl;
     for(i=0;i<fls;i++)
@@ -13,9 +67,11 @@ int __cargarTablaManual(tbl_val **tbl)
         clearScreen();
         printf("ingrese el valor %d de X: ", i+1);
         scanf("%lf", &(aux->x));
+        while (getchar() != '\n');
         clearScreen();
         printf("ingrese el valor %d de Y: ", i+1);
         scanf("%lf", &(aux->y));
+        while (getchar() != '\n');
         aux->result = NULL;
         aux++;
     }
@@ -31,9 +87,11 @@ int __cargarTablaSemiAuto(tbl_val **tbl)
     clearScreen();
     printf("Ingrese el valor de X: ");
     scanf("%lf", &x);
+    while (getchar() != '\n');
     clearScreen();
     printf("Ingrese el valor de Y: ");
     scanf("%lf", &y);
+    while (getchar() != '\n');
     for(i=0;i<fls;i++)
     {
         aux->x = x + (-5 + i);
@@ -71,22 +129,27 @@ void __mostrarResultados(char *func, tbl_val *tbl, int cant_fls)
     waitEnter();
 }
 
-void mainModuloF(char* fn)
+void mainModuloF()
 {
     tbl_val *tabla = NULL;
     tbl_val *aux_tbl;
     int len_tbl, i;
     char op;
+    char* fn;
     clearScreen();
+    fn = __seleccionarFuncion();
+    clearScreen();
+    printf("Ecuacion seleccionada: %s\n\n", fn);
     printf("Seleccione operacion a realizar:\n");
-    printf("* Resolver ecuacion con tabla manual de valores    --> a\n");
-    printf("* Resolver ecuacion con valor unico y rangos (+-5) --> b\n");
-    printf("* Volver al menu anterior                          --> x\n");
+    printf("* Resolver ecuacion con tabla manual de valores    --> A\n");
+    printf("* Resolver ecuacion con valor unico y rangos (+-5) --> B\n");
+    printf("* Volver al menu anterior                          --> X\n");
     printf("\nIngrese una opcion para continuar: ");
     scanf("%c", &op);
+    while (getchar() != '\n');
     switch(op)
     {
-        case 'a':
+        case 'A':
             len_tbl = __cargarTablaManual(&tabla);
             aux_tbl = tabla;
             //__mostrarTabla(tabla, len_tbl);
@@ -98,7 +161,8 @@ void mainModuloF(char* fn)
             __mostrarResultados(fn, tabla, len_tbl);
             free(tabla);
             mainModuloF(fn);
-        case 'b':
+            break;
+        case 'B':
             len_tbl = __cargarTablaSemiAuto(&tabla);
             aux_tbl = tabla;
             //__mostrarTabla(tabla, len_tbl);
@@ -110,8 +174,9 @@ void mainModuloF(char* fn)
             __mostrarResultados(fn, tabla, len_tbl);
             free(tabla);
             mainModuloF(fn);
-        case 'x': // es el valor de x traducido a int
-            return;
+            break;
+        case 'X': // es el valor de x traducido a int
+            break;
         default:
             clearScreen();
             printf("Operacion invalida, ingrese una opcion correcta\n");
